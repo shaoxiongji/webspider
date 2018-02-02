c45c0ca03efa59a87a9fff885c8f0f16ce0782b0
shaoxiongji <shaoxiong.ji@foxmail.com>
2018-02-02 16:57:52 +1100

    clean

--- redditscrapy/scrapy_reddit.py
+++ redditscrapy/scrapy_reddit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 8/09/17 2:27 PM
-# @Author  : Shaoxiong Ji
-# @Software: PyCharm
-# @Version : 3.6.0
-# @scrapy  : 1.3.3
+# Python version: 3.6
 
 from datetime import date
 from scrapy import cmdline
@@ -13,5 +9,3 @@ today = date.today().strftime("%Y%m%d")
 
 subreddit = "popular"
 cmdline.execute("scrapy crawl reddit -o ../crawl_neg/reddit_{}_{}.json".format(subreddit, today).split())
-
-
